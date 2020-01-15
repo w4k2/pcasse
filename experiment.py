e159e5d51462f3abc43d40f47c88c817bfb7ee40
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2020-01-15 18:57:56 +0100

    EXP

--- experiment.py
+++ experiment.py
@@ -1,3 +1,6 @@
+"""
+Ustalona liczba komponentów.
+"""
 from PCASSE import PCASSE, PCASSEE
 from sklearn.base import clone
 from sklearn.datasets import make_classification
@@ -6,26 +9,31 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import numpy as np
 
+
 print("LOADED")
 # Configure
-n_components = 20
+n_components = 25
 clfs = {
     "SVC": SVC(),
-    "PCASSE": PCASSE(subspace_size=4, n_components=n_components),
-    "PCASSEE": PCASSEE(subspace_size=4, distribuant_treshold=0.2),
+    "PCASSE-Oracle": PCASSE(subspace_size=4, n_components=n_components),
+    "PCASSE.1": PCASSEE(subspace_size=4, distribuant_treshold=0.1),
+    "PCASSE.2": PCASSEE(subspace_size=4, distribuant_treshold=0.2),
+    "PCASSE.3": PCASSEE(subspace_size=4, distribuant_treshold=0.3),
 }
 n_splits = 5
-repetitions = 10
+repetitions = 1
 
+print(clfs.keys())
 clf = None
 # Gather dataset
-for n_features in range(1000, 50000, 1000):
+for n_features in range(500, 15000, 500):
     scores = np.zeros((len(clfs), n_splits, repetitions))
     for repetition, random_state in enumerate(range(repetitions)):
         X, y = make_classification(
-            n_samples=250,
+            n_samples=500,
             n_features=n_features,
             n_informative=n_components,
+            n_clusters_per_class=1,
             n_redundant=0,
             random_state=42 + random_state,
         )
@@ -47,5 +55,5 @@ for n_features in range(1000, 50000, 1000):
         "%5i" % n_features,
         mean_scores,
         # "%.3f" % (mean_scores[0] - mean_scores[1]),
-        clf.n_components,
+        # clf.n_components,
     )
