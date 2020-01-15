b2e7480111033a059392ef3d7579a8626b31bd2b
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2020-01-15 18:22:57 +0100

    PCASSEE

--- PCASSE.py
+++ PCASSE.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
+
 class PCASSE(ClassifierMixin, BaseEstimator):
     def __init__(self, subspace_size=None, n_components=4):
         self.subspace_size = subspace_size
@@ -10,7 +11,51 @@ class PCASSE(ClassifierMixin, BaseEstimator):
 
     def fit(self, X, y, classes=None):
         # Calculate PCA components
-        components = np.abs(PCA(n_components=self.n_components, svd_solver="full").fit(X).components_)
+        components = np.abs(
+            PCA(n_components=self.n_components, svd_solver="full").fit(X).components_
+        )
+        # print("A ", components.shape)
+
+        # Gather ensemble
+        self.subspaces = np.array(
+            [np.argsort(-row)[: self.subspace_size] for row in components]
+        )
+
+        # Build ensemble
+        self.ensemble = [SVC().fit(X[:, subspace], y) for subspace in self.subspaces]
+
+        return self
+
+    def predict(self, X):
+        return (
+            np.mean(
+                np.array(
+                    [
+                        self.ensemble[i].decision_function(X[:, subspace])
+                        for i, subspace in enumerate(self.subspaces)
+                    ]
+                ),
+                axis=0,
+            )
+            > 0
+        ).astype(int)
+
+
+class PCASSEE(ClassifierMixin, BaseEstimator):
+    def __init__(self, subspace_size=None, distribuant_treshold=0.1):
+        self.subspace_size = subspace_size
+        self.distribuant_treshold = distribuant_treshold
+
+    def fit(self, X, y, classes=None):
+        # Calculate PCA components
+
+        pca = PCA(svd_solver="full").fit(X)
+        components = np.abs(pca.components_)
+
+        # Z EVR
+        evrd = np.add.accumulate(pca.explained_variance_ratio_)
+        self.n_components = np.where(evrd > self.distribuant_treshold)[0][0]
+        components = components[: self.n_components, :]
 
         # Gather ensemble
         self.subspaces = np.array(
--- experiment.py
+++ experiment.py
@@ -1,37 +1,51 @@
-from PCASSE import PCASSE
+from PCASSE import PCASSE, PCASSEE
 from sklearn.base import clone
 from sklearn.datasets import make_classification
 from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import numpy as np
+
 print("LOADED")
 # Configure
+n_components = 20
 clfs = {
-    'PCASSE': PCASSE(subspace_size=4, n_components=2),
-    'SVC': SVC()
+    "SVC": SVC(),
+    "PCASSE": PCASSE(subspace_size=4, n_components=n_components),
+    "PCASSEE": PCASSEE(subspace_size=4, distribuant_treshold=0.2),
 }
 n_splits = 5
-repetitions = 1
+repetitions = 10
 
+clf = None
 # Gather dataset
-for n_features in range(100,10000,100):
-    scores = np.zeros((len(clfs),n_splits, repetitions))
+for n_features in range(1000, 50000, 1000):
+    scores = np.zeros((len(clfs), n_splits, repetitions))
     for repetition, random_state in enumerate(range(repetitions)):
-        X, y = make_classification(n_samples=250, n_features=n_features, random_state=42+random_state)
-
+        X, y = make_classification(
+            n_samples=250,
+            n_features=n_features,
+            n_informative=n_components,
+            n_redundant=0,
+            random_state=42 + random_state,
+        )
 
         # Perform CV
         skf = StratifiedKFold(n_splits=n_splits)
-        for split, (train, test) in enumerate(skf.split(X,y)):
+        for split, (train, test) in enumerate(skf.split(X, y)):
             # Iterate classifiers
             for clf_idx, clf_n in enumerate(clfs):
                 clf = clone(clfs[clf_n]).fit(X[train], y[train])
                 y_pred = clf.predict(X[test])
                 score = accuracy_score(y[test], y_pred)
-                scores[clf_idx,split,repetition] = score
+                scores[clf_idx, split, repetition] = score
 
     mean_repeated_scores = np.mean(scores, axis=2)
     mean_scores = np.mean(mean_repeated_scores, axis=1)
 
-    print("%3i" % n_features, mean_scores, "%.3f" % (mean_scores[0]-mean_scores[1]))
+    print(
+        "%5i" % n_features,
+        mean_scores,
+        # "%.3f" % (mean_scores[0] - mean_scores[1]),
+        clf.n_components,
+    )
